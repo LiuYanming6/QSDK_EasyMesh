4f6719f9126f4c6cd5a1761eb9d7a108309eb849
EAP6666 <eap6666@eap.com>
2019-12-12 15:09:52 +0800

    [twbencode]Fix incorrect length when get sha256 encode

--- qsdk/package/twb/twbencode/src/ril_crypto.c
+++ qsdk/package/twb/twbencode/src/ril_crypto.c
@@ -135,7 +135,7 @@ void sha256(char *string, char outputBuffer[65])
     unsigned char hash[SHA256_DIGEST_LENGTH];
     SHA256_CTX sha256;
     SHA256_Init(&sha256);
-    SHA256_Update(&sha256, string, strlen(string));
+    SHA256_Update(&sha256, string, 32);
     SHA256_Final(hash, &sha256);
     int i = 0;
     for(i = 0; i < SHA256_DIGEST_LENGTH; i++)
