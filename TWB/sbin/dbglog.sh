0c1ed27c52f209576badf25f96a2372bfc33cd5a
EAP5369 <eap5369@eap.com>
2020-05-29 18:49:44 +0800

    [TWB][sbin] Add more information in script for ACS debug log.

--- TWB/sbin/dbglog.sh
+++ TWB/sbin/dbglog.sh
@@ -10,3 +10,23 @@ echo "|----------- kernel -----------|" >> $DBG_LOG
 dmesg >> $DBG_LOG
 echo "|----------- run-time system  -----------|" >> $DBG_LOG
 logread -F $DBG_LOG
+echo "|----------- uci parameters  -----------|" >> $DBG_LOG
+uci show >> $DBG_LOG
+echo "|----------- wireless interfaces information  -----------|" >> $DBG_LOG
+iwconfig >> $DBG_LOG
+echo "|----------- interfaces information  -----------|" >> $DBG_LOG
+ifconfig >> $DBG_LOG
+echo "|----------- 2.4G Fronthaul AP client information  -----------|" >> $DBG_LOG
+wlanconfig ath0 list >> $DBG_LOG
+echo "|----------- 2.4G Backhaul AP client information  -----------|" >> $DBG_LOG
+wlanconfig ath01 list >> $DBG_LOG
+echo "|----------- 5G Backhaulhaul STA information  -----------|" >> $DBG_LOG
+wlanconfig ath1 list >> $DBG_LOG
+echo "|----------- 5G Fronthaul AP client information  -----------|" >> $DBG_LOG
+wlanconfig ath11 list >> $DBG_LOG
+echo "|----------- 5G Backhaul AP client information  -----------|" >> $DBG_LOG
+wlanconfig ath12 list >> $DBG_LOG
+echo "|----------- run-time process  -----------|" >> $DBG_LOG
+ps -w >> $DBG_LOG
+echo "|----------- Route information  -----------|" >> $DBG_LOG
+route >> $DBG_LOG
